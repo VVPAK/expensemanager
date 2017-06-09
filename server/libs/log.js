aefe22318ae8fc7cfd4cadfe02682509442c61b2
VVPAK <vvpak14@gmail.com>
2017-06-09 19:06:05 +0600

    Loging changed

--- server/libs/log.js
+++ server/libs/log.js
@@ -9,16 +9,6 @@ function getLogger(module) {
                 colorize:   true,
                 level:      'debug',
                 label:      path
-            }),
-            new (winston.transports.File)({
-                name: 'info-file',
-                filename: 'filelog-info.log',
-                level: 'info'
-            }),
-            new (winston.transports.File)({
-                name: 'error-file',
-                filename: 'filelog-error.log',
-                level: 'error'
             })
         ]
     });
